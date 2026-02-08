d231b68a2760dab062d329e5ae989c83b1dc5d3f
Oluwatunmise Alimi <oluwatunmiselaimi67@gmail.com>
2026-02-08 21:00:37 +0100

    string loop

---
+++ StringLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace StringLoop;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string msg = "C# is awesome";
+        for(int i = 0; i < msg.Length; i++)
+        {
+            Console.Write(msg[i]);
+            Thread.Sleep(100);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine(msg.Contains("C"));
+
+        bool contains = false;
+        for(int i = 0; i < msg.Length; i++)
+        {
+            if(msg[i] == 'C')
+            {
+                contains = true;
+            }
+        }
+        Console.WriteLine(contains);
+        
+    }
+}
