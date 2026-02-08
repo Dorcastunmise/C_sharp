49b48b32ec6f32ef9b324d0f714198dae61b2770
Oluwatunmise Alimi <oluwatunmiselaimi67@gmail.com>
2026-02-08 21:41:51 +0100

    null or empty

---
+++ IsNullOrEmpty/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace IsNullOrEmpty;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        if(name.Equals("")) Console.WriteLine("0");
+        else Console.WriteLine("1");
+
+        if(!string.IsNullOrEmpty(name)) Console.WriteLine("2"); //IsNullOrEmpty checks for "" and null as well 
+    }
+}
