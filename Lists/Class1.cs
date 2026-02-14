d92fe3e1aff17f9bcd53c70811b7859db4e366d5
Oluwatunmise Alimi <oluwatunmiselaimi67@gmail.com>
2026-02-14 12:12:59 +0100

    Lists

---
+++ Lists/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace Lists;
+
+public class Class1
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>();
+        
+        for(int i =0; i < 3; i++)
+        {
+            Console.Write("Enter a number:");
+            numbers.Add(Convert.ToInt32(Console.ReadLine()));
+        }
+
+        for(int n = 0; n <= numbers.Count; n++)
+        {
+            Console.WriteLine(numbers[n]);   
+        }
+
+        numbers.RemoveAt(0);
+        foreach(int n in numbers)
+        {
+            Console.WriteLine(n);
+        }
+
+    }
+}
