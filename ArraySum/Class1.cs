315c7a104d45aab8428243ee41c3e87c6daa567d
Oluwatunmise Alimi <oluwatunmiselaimi67@gmail.com>
2026-02-14 23:28:06 +0100

    array calculations

---
+++ ArraySum/Class1.cs
@@ -0,0 +1,34 @@
+﻿namespace ArraySum;
+
+public class Class1
+{
+     static void Main(string[] args)
+    {
+        int[] numbers = new int[]
+        {
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        };
+
+        bool result = ArraySum(numbers, out int sum);
+        if(result) Console.WriteLine($" The total is {sum}");
+        else Console.WriteLine("Cannot add up an empty array!");
+    }
+
+    static bool ArraySum(int[] numbers, out int sum)
+    {
+        sum = 0;
+        if(numbers.Length > 0){
+           
+            foreach(int number in numbers)
+            {
+                sum += number;
+                //Console.WriteLine($"Sum: {sum} + {number}");
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ TraingleArea/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+
+namespace TraingleArea;
+
+public class Class1
+{
+    static void Main()
+    {
+        int width = MsgEnquiryNumbers("What is the width of the supposed Triangle");
+        int height = MsgEnquiryNumbers("What is the height of the supposed Triangle");
+
+        int area = Area(width, height);
+        if(area == 0) Console.WriteLine("Both the height and width values have to be greater than 0");
+        else Console.WriteLine($"The area of the triangle is {area}");
+    }
+
+
+    static int Area(int width, int height)
+    {
+        if(width > 0 && height > 0)
+        {
+            int area = width * height / 2;
+            return area;
+        }
+        return 0;
+    }
+
+    static int MsgEnquiryNumbers(string message)
+    {
+        Console.Write(message + ": ");
+        int intResponse = Convert.ToInt32(Console.ReadLine());
+        return intResponse;
+    }
+
+    static string MsgEnquiryText(string message)
+    {
+        Console.Write(message + ": ");
+        string response = Console.ReadLine();
+        return response;
+    }
+}
